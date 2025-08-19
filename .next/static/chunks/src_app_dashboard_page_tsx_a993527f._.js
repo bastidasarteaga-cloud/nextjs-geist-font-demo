92492ac0c460f20573728ae0cc9d472f5400abac
bastidasarteaga-cloud <bastidas.arteaga@gmail.com>
2025-08-19 10:14:25 -0400

    Add .next/static/chunks/src_app_dashboard_page_tsx_a993527f._.js 1755612864409

---
+++ .next/static/chunks/src_app_dashboard_page_tsx_a993527f._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_@supabase_node-fetch_browser_dce611a6.js",
+  "static/chunks/src_ea41df8a._.js",
+  "static/chunks/node_modules_277a9e24._.js"
+],
+    source: "dynamic"
+});
